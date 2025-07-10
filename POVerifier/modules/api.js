7de3eb9e76c9e5d13da874c16f73390e6cd8bd5f
ntwoods <mis01@ntwoods.com>
2025-07-10 15:48:16 +0530

    Create api.js

---
+++ POVerifier/modules/api.js
@@ -0,0 +1,2 @@
+export const API_URL = 'https://script.google.com/macros/s/AKfycbxnXt8Ti_RvyfVMRFGbZ-qCUAK-FhmRlN0-B1CDxdKGcVizExq6-MSNMHNCQIDekiiL/exec';
+export const API_URL2 = API_URL; // Same endpoint for now
